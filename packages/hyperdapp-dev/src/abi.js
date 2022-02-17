b772edb7ddc2d2842110ea43a030c914b796297a
Gilbert <gilbertbgarza@gmail.com>
2022-02-16 18:21:46 -0600

    convertABIToPrologCode: Support tuples

--- packages/hyperdapp-dev/src/abi.js
+++ packages/hyperdapp-dev/src/abi.js
@@ -12,15 +12,33 @@ export function convertABIToPrologCode(abi) {
     row += escapeAtom(name);
 
     if (inputs.length) {
-      row += "(";
-      for (let input of inputs) row += `${input.type}, `;
-      row = row.slice(0, -2);
-      row += ")";
+      row += '('
+      row += inputs
+        .map(input => {
+          let type = input.type
+
+          if (type === 'tuple') {
+            type += '('
+            type += input.components
+              .map(tupleType => {
+                if (tupleType.type === 'tuple') {
+                  throw new Error('Nested tuples currently not supported')
+                }
+                return tupleType.type
+              })
+              .join(', ')
+            type += ')'
+          }
+
+          return type
+        })
+        .join(', ')
+      row += ')'
     }
 
     if (outputs.length) row += `: ${outputs[0].type}`;
 
-    if (["view", "payable"].includes(stateMutability)) {
+    if (["pure", "view", "payable"].includes(stateMutability)) {
       if (row.indexOf(":") !== -1) row += ` / ${stateMutability}`;
       else row += `: ${stateMutability}`;
     }
