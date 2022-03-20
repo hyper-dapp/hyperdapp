6dacc75ced8774d3b3f79f2fbcfd20ff91442a53
samuel <sam.teboul@gmail.com>
2022-03-20 15:27:34 +0100

    Add redirectTo prop

--- packages/chatdapp/src/components/HyperdappLogo.tsx
+++ packages/chatdapp/src/components/HyperdappLogo.tsx
@@ -1,13 +1,21 @@
 import { useNavigate } from "react-router-dom";
 
-const HyperdappLogo = ({ clickable }: { clickable?: boolean }) => {
+const HyperdappLogo = ({
+  clickable,
+  redirectTo,
+}: {
+  clickable?: boolean;
+  redirectTo?: string;
+}) => {
   const navigate = useNavigate();
 
   return (
     <img
       className="cursor-pointer mr-8"
       src="/images/hyperdapp-logo.png"
-      onClick={() => (clickable ? navigate("/") : null)}
+      onClick={() =>
+        clickable ? navigate(redirectTo ? redirectTo : "/") : null
+      }
       width="170"
       height="70"
       alt="hyperdapp-logo"
